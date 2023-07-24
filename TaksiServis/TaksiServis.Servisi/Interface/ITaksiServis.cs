6c2347941d4f91b37c204d6694b1c7d299d68b9c
Ognjen Grgur <ognjen.grgur12@gmail.com>
2023-07-24 19:06:51 +0200

    inerface

---
+++ TaksiServis/TaksiServis.Servisi/Interface/ITaksiServis.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaksiServis.Modeli;
+
+namespace TaksiServis.Servisi.Interface
+{
+    public interface ITaksiServis
+    {
+        Task<IEnumerable<Vozilo>> PrikazSvihVozilaAsync();
+        Task<IEnumerable<Vozilo>> prikazSvihVozilaPoRegistraciji(object registracija);
+        Task<IEnumerable<Vozilo>> prikazSvihVozilaPoModelu(object model);
+        Task AzurirajFilm(Vozilo obj, object ID);
+        Task KreirajNovFilm(Vozilo KartaModel);
+        void ObrisiFilm(object ID);
+        bool PostojiFilmUBaziPodataka();
+    }
+}
