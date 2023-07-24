f3db16af89df82345dc1260e2b3f254666976709
Ognjen Grgur <ognjen.grgur12@gmail.com>
2023-07-24 19:46:43 +0200

    Startup Fix

--- TaksiServis/TaksiServis.KorisnickiInterfejs/Program.cs
+++ TaksiServis/TaksiServis.KorisnickiInterfejs/Program.cs
@@ -1,17 +1,38 @@
+using Autofac;
+using System.ComponentModel;
+using TaksiServis.Repozitoriumi.Context;
+using TaksiServis.Repozitoriumi.Interfejsi;
+using TaksiServis.Repozitoriumi.Repozitorijumi;
+using TaksiServis.Servisi.Interface;
+using TaksiServis.Servisi.Servisi;
+
 namespace TaksiServis.KorisnickiInterfejs
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
+        private static Autofac.IContainer _container;
+
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Build the dependency injection container
+            var builder = new ContainerBuilder();
+            builder.RegisterType<TaksiDBCotext>().InstancePerLifetimeScope();
+            builder.RegisterType<Servisi.Servisi.TaksiServis>().As<ITaksiServis>();
+            builder.RegisterType<TaksiRepozitorijum>().As<ITaksiRepozitory>();
+            builder.RegisterType<Form1>().As<Form>();
+            // Register other dependencies here
+            _container = builder.Build();
+
+            // Resolve the main form from the container
+            using (var form = _container.Resolve<Form>())
+            {
+                Application.Run(form);
+            }
+
         }
     }
 }
