0c0b35981de080d642ef94e6a6bb6ef072558b4c
Ognjen Grgur <ognjen.grgur12@gmail.com>
2023-07-24 19:42:21 +0200

    fix

--- TaksiServis/TaksiServis.KorisnickiInterfejs/Form1.cs
+++ TaksiServis/TaksiServis.KorisnickiInterfejs/Form1.cs
@@ -1,7 +1,10 @@
+using TaksiServis.Servisi.Interface;
+
 namespace TaksiServis.KorisnickiInterfejs
 {
     public partial class Form1 : Form
     {
+        private readonly ITaksiServis taksiServis;
         public Form1()
         {
             InitializeComponent();
