bc69ef09082eb732e74ba646fd6b347116bc7390
Ognjen Grgur <ognjen.grgur12@gmail.com>
2023-08-07 19:14:36 +0200

    Mid save

--- TaksiServis/TaksiServis.KorisnickiInterfejs/Form1.cs
+++ TaksiServis/TaksiServis.KorisnickiInterfejs/Form1.cs
@@ -11,12 +11,33 @@ namespace TaksiServis.KorisnickiInterfejs
         {
             InitializeComponent();
             _taksiServis = taksiServis;
-            Prikaz();
+            _ = Prikaz();
+            fillCbx();
+        }
+
+        private void fillCbx()
+        {
+            cbxFilter.Items.Clear();
+
+            cbxFilter.Items.Add("Po Registraciji");
+            cbxFilter.Items.Add("Po Modelu");
         }
 
         private void TableView_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+            try
+            {
+                DataGridViewRow selektovaniRed = TableView.Rows[e.RowIndex];
+
+                txtID.Text = selektovaniRed.Cells["ID"].Value.ToString();
+                txtMarka.Text = selektovaniRed.Cells["Marka"].Value.ToString();
+                txtModel.Text = selektovaniRed.Cells["Model"].Value.ToString();
+                txtGodiste.Text = selektovaniRed.Cells["Godiste"].Value.ToString();
+                txtRegistracija.Text = selektovaniRed.Cells["Registracija"].Value.ToString();
+
+            }
+            catch (Exception ex) { }
         }
 
         private async Task Prikaz()
@@ -41,6 +62,7 @@ namespace TaksiServis.KorisnickiInterfejs
                 {
                     Marka = txtMarka.Text,
                     Model = txtModel.Text,
+                    Gdiste = DateTime.Parse(txtGodiste.Text),
                     Registracija = txtRegistracija.Text,
                 };
 
@@ -52,6 +74,64 @@ namespace TaksiServis.KorisnickiInterfejs
                 MessageBox.Show(ex.ToString());
             }
             _ = Prikaz();
+        }
+
+        private async void btnIzmeni_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int id = int.Parse(txtID.Text);
+
+                Vozilo VoziloZaIzmeniti = new()
+                {
+                    Marka = txtMarka.Text,
+                    Model = txtModel.Text,
+                    Gdiste = DateTime.Parse(txtGodiste.Text),
+                    Registracija = txtRegistracija.Text,
+                };
+
+
+                await _taksiServis.AzurirajVozilo(VoziloZaIzmeniti, id);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            _ = Prikaz();
+        }
+
+        private void btnObrisi_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int id = int.Parse(txtID.Text);
+                _taksiServis.ObrisiVozilo(id);
+            }
+            catch (Exception ex) { }
+            _ = Prikaz();
+        }
+
+        private async void btnFiltriraj_ClickAsync(object sender, EventArgs e)
+        {
+            var selected = cbxFilter.SelectedItem;
+            var value = txtFilterBox.Text;
+            var vozilo = await _taksiServis.PrikazSvihVozilaAsync();
+            try
+            {
+                switch (selected)
+                {
+                    case "Po Registraciji":
+                        vozilo = await _taksiServis.prikazSvihVozilaPoRegistraciji(value);
+                        break;
+                    case "Po Modelu":
+                        vozilo = await _taksiServis.prikazSvihVozilaPoModelu(value);
+                        break;
+                    default:
+                        break;
+                }
+                TableView.DataSource = vozilo;
+            }
+            catch (Exception ex) { }
         }
     }
 }
